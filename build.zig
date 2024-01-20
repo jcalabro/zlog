6fc419cf565ee222b00a5eddb5916326af7aef29
jcalabro <jamesrcalabro@gmail.com>
2024-01-20 05:55:56 -0500

    rename module

--- build.zig
+++ build.zig
@@ -1,25 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const logger = b.addModule("logger", .{
-        .root_source_file = .{ .path = "src/logger.zig" },
+    const zlog = b.addModule("zlog", .{
+        .root_source_file = .{ .path = "src/zlog.zig" },
     });
 
     const time_dep = b.dependency("time", .{});
     const time_mod = time_dep.module("time");
-    logger.addImport("time", time_mod);
+    zlog.addImport("time", time_mod);
 
     {
         const target = b.standardTargetOptions(.{});
         const optimize = b.standardOptimizeOption(.{});
 
         const tests = b.addTest(.{
-            .root_source_file = .{ .path = "src/logger.zig" },
+            .root_source_file = .{ .path = "src/zlog.zig" },
             .target = target,
             .optimize = optimize,
         });
 
-        tests.root_module.addImport("logger", logger);
+        tests.root_module.addImport("zlog", zlog);
         tests.root_module.addImport("time", time_mod);
 
         const run_tests = b.addRunArtifact(tests);
