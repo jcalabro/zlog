9d3fcb3dbea9e7531b4e4d7f1cc166cf9ebd83c3
jcalabro <jamesrcalabro@gmail.com>
2024-06-23 09:42:38 -0400

    buffered logging

--- src/zlog.zig
+++ src/zlog.zig
@@ -8,16 +8,16 @@ const testing = std.testing;
 
 const time = @import("time");
 
-pub const Level = enum(u8) {
-    const Self = @This();
+const BufferedWriter = io.BufferedWriter(4096, fs.File.Writer);
 
+pub const Level = enum(u8) {
     dbg,
     inf,
     wrn,
     err,
     ftl,
 
-    fn enabled(global_lvl: Self, local_lvl: Self) bool {
+    fn enabled(global_lvl: @This(), local_lvl: @This()) bool {
         return @intFromEnum(global_lvl) <= @intFromEnum(local_lvl);
     }
 };
@@ -42,13 +42,17 @@ pub const Options = struct {
     active_regions: [][]const u8 = undefined,
 
     fp: fs.File,
+    buf: BufferedWriter = undefined,
 
     color: bool = true,
 
     // only one thread is allowed to write to the output file at a time
     mu: Mutex = Mutex{},
 
-    pub fn deinit(self: @This()) void {
+    pub fn deinit(self: *@This()) void {
+        // attempt a best-effort flush before shutting down
+        self.buf.flush() catch {};
+
         self.allocator.free(self.active_regions);
     }
 };
@@ -58,6 +62,7 @@ var opts: Options = undefined;
 
 pub fn init(op: Options) !void {
     opts = op;
+    opts.buf = io.bufferedWriter(opts.fp.writer());
 
     opts.active_regions = blk: {
         var active_regions = std.ArrayList([]const u8).init(opts.allocator);
@@ -157,48 +162,53 @@ pub const Logger = struct {
         opts.mu.lock();
         defer opts.mu.unlock();
 
-        opts.fp.writeAll(output) catch return;
+        opts.buf.writer().writeAll(output) catch return;
     }
 
-    pub fn debug(self: @This(), comptime fmt: []const u8) void {
+    pub fn debug(self: Self, comptime fmt: []const u8) void {
         debugf(self, fmt, .{});
     }
 
-    pub fn debugf(self: @This(), comptime fmt: []const u8, args: anytype) void {
+    pub fn debugf(self: Self, comptime fmt: []const u8, args: anytype) void {
         write(self, fmt, args, Level.dbg, Color.Green);
     }
 
-    pub fn info(self: @This(), comptime fmt: []const u8) void {
+    pub fn info(self: Self, comptime fmt: []const u8) void {
         infof(self, fmt, .{});
     }
 
-    pub fn infof(self: @This(), comptime fmt: []const u8, args: anytype) void {
+    pub fn infof(self: Self, comptime fmt: []const u8, args: anytype) void {
         write(self, fmt, args, Level.inf, Color.Blue);
     }
 
-    pub fn warn(self: @This(), comptime fmt: []const u8) void {
+    pub fn warn(self: Self, comptime fmt: []const u8) void {
         warnf(self, fmt, .{});
     }
 
-    pub fn warnf(self: @This(), comptime fmt: []const u8, args: anytype) void {
+    pub fn warnf(self: Self, comptime fmt: []const u8, args: anytype) void {
         write(self, fmt, args, Level.wrn, Color.Yellow);
     }
 
-    pub fn err(self: @This(), comptime fmt: []const u8) void {
+    pub fn err(self: Self, comptime fmt: []const u8) void {
         errf(self, fmt, .{});
     }
 
-    pub fn errf(self: @This(), comptime fmt: []const u8, args: anytype) void {
+    pub fn errf(self: Self, comptime fmt: []const u8, args: anytype) void {
         write(self, fmt, args, Level.err, Color.Red);
     }
 
-    pub fn fatal(self: @This(), comptime fmt: []const u8) noreturn {
+    pub fn fatal(self: Self, comptime fmt: []const u8) noreturn {
         fatalf(self, fmt, .{});
     }
 
-    pub fn fatalf(self: @This(), comptime fmt: []const u8, args: anytype) noreturn {
+    pub fn fatalf(self: Self, comptime fmt: []const u8, args: anytype) noreturn {
         write(self, fmt, args, Level.ftl, Color.Red);
+        self.flush();
         std.process.exit(1);
+    }
+
+    pub fn flush(_: Self) void {
+        opts.buf.flush() catch return;
     }
 };
 
@@ -212,10 +222,7 @@ test "logging" {
     defer fp.close();
 
     {
-        //
-        // Write some data
-        //
-
+        // write some data
         try init(.{
             .allocator = testing.allocator,
             .level = .wrn,
@@ -231,13 +238,11 @@ test "logging" {
         log.info("world");
         log.warnf("testing {d}", .{123});
         log.err("final");
+        log.flush();
     }
 
     {
-        //
-        // Read the data and ensure it contains what we expect
-        //
-
+        // read the data and ensure it contains what we expect
         try fp.sync();
         try fp.seekTo(0);
 
@@ -261,5 +266,62 @@ test "logging" {
                 else => unreachable,
             };
         }
+    }
+}
+
+test "logging flushes" {
+    var tmp_dir = testing.tmpDir(.{});
+    defer tmp_dir.cleanup();
+
+    {
+        const fp = try tmp_dir.dir.createFile("1.txt", .{
+            .read = true,
+        });
+        defer fp.close();
+
+        // write some data (no flush should happen yet)
+        try init(.{
+            .allocator = testing.allocator,
+            .level = .dbg,
+            .regions = "all",
+            .fp = fp,
+            .color = false,
+        });
+        defer deinit();
+
+        const log = Logger.init("myregion");
+
+        log.debugf("{x}", .{1});
+        log.debugf("{x}", .{2});
+
+        // the buffer should not have flushed yet
+        try fp.seekTo(0);
+        try testing.expect(try fp.getEndPos() == 0);
+    }
+
+    {
+        const fp = try tmp_dir.dir.createFile("2.txt", .{
+            .read = true,
+        });
+        defer fp.close();
+
+        // write some more data (should trigger a flush)
+        try init(.{
+            .allocator = testing.allocator,
+            .level = .dbg,
+            .regions = "all",
+            .fp = fp,
+            .color = false,
+        });
+        defer deinit();
+
+        const log = Logger.init("myregion");
+
+        // should be way more than enough
+        for (0..10000) |ndx| log.debugf("{x}", .{ndx});
+
+        // the buffer should be flushed to disk
+        try fp.seekTo(0);
+        try testing.expect(try fp.getEndPos() > 0);
     }
 }
